3f1881b277d9d1dc463ef845e81af93b5b748ff7
Vlad Tagintsev <fiend3d@ya.ru>
2026-07-04 18:26:04 +0300

    updateStart on resize

--- update.go
+++ update.go
@@ -11,6 +11,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
+		m.updateStart()
 		return m, nil
 
 	case tea.MouseClickMsg:
